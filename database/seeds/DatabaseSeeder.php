7b438d6c588c786234c108a7fd0f8aba891c7070
Viktoriia <viktoriia.sarapii@devit-team.com>
2021-02-10 20:36:01 +0000

    Menambahkan profil System Administrator ketika seeding database pertama kali.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,15 +1,21 @@
 <?php
 use App\User;
+use App\Models\Profile;
 use Illuminate\Database\Seeder;
 class DatabaseSeeder extends Seeder
 {
     public function run()
     {
+        $new_role = new Profile([
+            'name' => 'System Administrator',
+        ]);
+        $new_role->save();
         $new_user = new User([
-        	'name' => 'System Administrator',
-        	'email' => 'admin@admin.com',
-        	'username' => 'admin',
-        	'password' => bcrypt('password'),
+            'profile_id' => 1,
+        	'name'       => 'System Administrator',
+        	'email'      => 'admin@admin.com',
+        	'username'   => 'admin',
+        	'password'   => bcrypt('password'),
         ]);
         $new_user->save();
     }
